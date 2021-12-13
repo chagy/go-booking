689ae724a0d9656e675a39df546e6d643b94e60a
chagy <chagy_x_@hotmail.com>
2021-12-13 10:48:56 +0700

    42

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/chagy/go-booking/pkg/config"
+	"github.com/chagy/go-booking/pkg/models"
+	"github.com/chagy/go-booking/pkg/render"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hollo"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	// m.App.Session.
+
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
